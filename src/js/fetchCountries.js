31adf832dc4c0c58a7f21a75d3434b4bac3f0ca4
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2022-10-16 20:39:26 +0300

    fetch, catch changed

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,7 +2,7 @@
 
 //import {countryList, countryInfo} from "./refs";
 
-import { emptyMarkup } from "./markupFunctions";  //  рендер пустої розмітки
+//import { emptyMarkup } from "./markupFunctions";  //  рендер пустої розмітки
 import Notiflix from "notiflix";
 
 const URL = 'https://restcountries.com/v3.1/name';
@@ -12,21 +12,16 @@ export function fetchCountries(name){
     return fetch(`${URL}/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
 
-            if (!response.ok) {
-                throw new Error()
+            if (!response.ok || response.status === 404) {
+                throw new Error('No matches found. Please enter correct name.');
             }
-            
-            if (response.status === 404){
-
-                Notiflix.Notify.warning('No matches found. Please enter correct name.');
-
-                throw new Error();
-            }
-            
             return response.json();
         })
         .catch(error => {
-            emptyMarkup();//рендерить пусту розмітку
+
+            Notiflix.Notify.warning(error);
+
+            //();//рендерить пусту розмітку
             console.log(error);
     })
 }
